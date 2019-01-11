684bad51285f6ff509db6801bf8c49a66dc908b1
Danilo Domingos <d.domingos@comunique-se.com.br>
2019-01-11 17:53:29 -0200

    [S5][44] - Tratamento de Erros com o Operador Catch.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,7 +1,9 @@
+import { ErrorHandler } from './../app.error-handler';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { MEAT_API } from './../app.api';
 import { Restaurant } from './restaurant/restaurant.model';
@@ -15,7 +17,8 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
 
     return this.http.get(`${MEAT_API}/restaurants`)
-      .map(resp => resp.json());
+      .map(resp => resp.json())
+      .catch(ErrorHandler.handleError);
   }
 
 }
