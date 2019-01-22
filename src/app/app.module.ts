eaffe5cb4a44167750b07e49b3501e4150d9b759
Danilo Domingos <d.domingos@comunique-se.com.br>
2019-01-22 17:23:19 -0200

    [S8][79] - Criando um Core Module (Services).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule, LOCALE_ID } from '@angular/core';
@@ -8,17 +9,16 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { SharedModule } from './shared/shared/shared.module';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+
+
+import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -35,14 +35,13 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
     ],
     imports: [
         BrowserModule,
-        SharedModule,
+        CoreModule,
         HttpModule,
-        RouterModule.forRoot(ROUTES)
+        RouterModule.forRoot(ROUTES),
+        SharedModule,
     ],
     providers: [
-        {provide: LOCALE_ID, useValue: 'pt-BR'},
-        RestaurantsService,
-        ShoppingCartService,
+        {provide: LOCALE_ID, useValue: 'pt-BR'}
     ],
     bootstrap: [AppComponent]
 })
