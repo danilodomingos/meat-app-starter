b5d99583003e895f8820fd396f55cca2653f0fae
Danilo Domingos <danilo.domingos02@gmail.com>
2019-02-10 14:11:12 -0200

    [S9][88] - Usando os operadores Do e SwitchMap.

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,42 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/timer';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
-import { NotificationService } from './../notification.service';
+import {NotificationService} from '../notification.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
   styleUrls: ['./snackbar.component.css'],
   animations: [
-      trigger('snack-visibility', [
-          state('hidden', style({
-              opacity: 0,
-              bottom: '0px'
-          })),
-          state('visible', style({
-            opacity: 1,
-            bottom: '30px'
-          })),
-          transition('hidden => visible', animate('500ms 0s ease-in')),
-          transition('visible => hidden', animate('500ms 0s ease-out'))
-      ])
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        bottom: '30px'
+      })),
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
+    ])
   ]
 })
 export class SnackbarComponent implements OnInit {
 
-    message: string;
-    snackVisibility = 'hidden';
+  message: string;
+  snackVisibility = 'hidden';
 
-    constructor(private notificationService: NotificationService) { }
+  constructor(private notificationService: NotificationService) { }
 
-    ngOnInit() {
-        this.notificationService.notifier.subscribe((message: string) => {
-            this.message = message;
-            this.snackVisibility = 'visible';
-            Observable.timer(3000).subscribe(time => this.snackVisibility = 'hidden');
-        });
-    }
+  ngOnInit() {
+      this.notificationService.notifier
+      .do((message: string) => {
+        this.message = message;
+        this.snackVisibility = 'visible';
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibility = 'hidden');
+  }
 
 }
